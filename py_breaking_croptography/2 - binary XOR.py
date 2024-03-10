1b96d9c221dced6fd5799fb0bcd03289117dbfb7
mrliber8 <patricktenbrinke@gmail.com>
2024-03-10 13:37:01 +0100

    Opdr2 af en 3bijna

--- py_breaking_croptography/2 - binary XOR.py
+++ py_breaking_croptography/2 - binary XOR.py
@@ -16,6 +16,14 @@ def fixed_length_xor(text, key):
     bytes
         binary XOR of text & key
     """
+
+    str = []
+    for x in range(len(text)):
+        str.append(text[x] ^key[x])
+
+    xor_output = bytes(str)
+    
+    #xor_output = bytes([a ^ b for a, b in zip(text, key)])
     
     return xor_output
 
@@ -39,10 +47,24 @@ def repeating_key_xor(text, key):
     bytes
         binary XOR of text & key
     """
+    counter = 0
+    
+    key = bytearray(key)
+    while len(text) != len(key):
+        key.append(key[counter])
+        counter += 1    
+
+    str = []
+    for x in range(len(text)):
+        str.append(text[x] ^key[x])
+
+    xor_output = bytes(str)
 
     return xor_output
 
 # Laat deze asserts onaangetast!
+
 assert type(repeating_key_xor(b'all too many words',b'bar')) == bytes
 assert b64encode(repeating_key_xor(b'all too many words',b'bar'))\
    == b'Aw0eQhUdDUEfAw8LQhYdEAUB'
+
--- py_breaking_croptography/3 - ECB Mode AES.py
+++ py_breaking_croptography/3 - ECB Mode AES.py
@@ -1,3 +1,4 @@
+from Crypto.Cipher import AES
 from base64 import b64decode
 
 def ECB_decrypt(ciphertext, key):
@@ -18,6 +19,8 @@ def ECB_decrypt(ciphertext, key):
         decrypted plaintext
     """
 
+    cipher = AES.new(key, AES.MODE_ECB)
+    plaintext = cipher.decrypt(ciphertext)
     return plaintext
 
 
@@ -26,3 +29,35 @@ ciphertext = b64decode('86ueC+xlCMwpjrosuZ+pKCPWXgOeNJqL0VI3qB59SSY=')
 key = b'SECRETSAREHIDDEN'
 assert ECB_decrypt(ciphertext, key)[:28] == \
     b64decode('SGFzdCBkdSBldHdhcyBaZWl0IGZ1ciBtaWNoPw==')
+
+
+with open("py_breaking_croptography/file3.txt", "r") as f:
+    file = f.read()
+
+ciphertext = b64decode(file)
+
+print(ECB_decrypt(ciphertext, key))
+
+def ECB_encrypt(plaintext, key):
+
+
+    return ciphertext
+
+
+
+
+'''
+https://www.techtarget.com/whatis/definition/ASCII-American-Standard-Code-for-Information-Interchange#:~:text=The%20standard%20ASCII%20character%20set%20is%20only%207%20bits%2C%20and,bit%20is%20set%20to%201.
+Een ASCII character is 7 bits, maar wordt weergeven als 8 bits waarbij 
+de voorste bit op 0 wordt gezet. Bij de extended ASCII lijst wordt de 
+voorste bit juist op 1 gezet. Zo is het euro teken bijvoorbeeld binary
+1000 0000.
+
+#Secretsarehidden is:
+print(len(key)) # 16 lang
+
+bit_lengte = len(key) * 8 
+print(bit_lengte) # 128
+
+Er wordt dus gebruik gemaakt van AES-128
+'''
