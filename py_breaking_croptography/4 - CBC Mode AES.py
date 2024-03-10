fd2bfcdfa80432d16972a9220bbcfe358992656a
mrliber8 <patricktenbrinke@gmail.com>
2024-03-10 18:51:07 +0100

    4 b,c

--- py_breaking_croptography/4 - CBC Mode AES.py
+++ py_breaking_croptography/4 - CBC Mode AES.py
@@ -1,4 +1,63 @@
+from Crypto.Cipher import AES
 from base64 import b64decode
+from base64 import b64encode
+
+
+def repeating_key_xor(text, key):
+    """Takes two bytestrings and XORs them, returning a bytestring.
+    Extends the key to match the text length.
+    
+    Parameters
+    ----------
+    text : bytes
+        bytes-object to be xor'd w/ key
+    key : bytes
+        bytes-object to be xor'd w/ text
+        
+    Returns
+    -------
+    bytes
+        binary XOR of text & key
+    """
+    counter = 0
+    
+    key = bytearray(key)
+    while len(text) != len(key):
+        key.append(key[counter])
+        counter += 1    
+
+    str = []
+    for x in range(len(text)):
+        str.append(text[x] ^key[x])
+
+    xor_output = bytes(str)
+
+    return xor_output
+
+
+def ECB_decrypt(ciphertext, key):
+    """Accepts a ciphertext in byte-form,
+    as well as 16-byte key, and returns 
+    the corresponding plaintext.
+
+    Parameters
+    ----------
+    ciphertext : bytes
+        ciphertext to be decrypted
+    key : bytes
+        key to be used in decryption
+
+    Returns
+    -------
+    bytes
+        decrypted plaintext
+    """
+
+    cipher = AES.new(key, AES.MODE_ECB)
+    plaintext = cipher.decrypt(ciphertext)
+    return plaintext
+
+
 
 def CBC_decrypt(ciphertext, key, IV):
     """Decrypts a given plaintext in CBC mode.
@@ -22,6 +81,22 @@ def CBC_decrypt(ciphertext, key, IV):
     bytes
         Decrypted plaintext
         """
+    
+
+    plaintext = b''
+    previous_block = IV
+
+    # First splits the ciphertext into keylength-size blocks,
+    for i in range(0, len(ciphertext), 16):
+        current_block = ciphertext[i:i+16]
+        # then decrypts them individually w/ ECB-mode AES
+        decrypted_block = ECB_decrypt(current_block, key)
+        # and XOR's each result with either the IV or the previous ciphertext block.
+        plaintext_block = repeating_key_xor(decrypted_block, previous_block)
+        # Appends decrypted blocks together for the output.
+        plaintext += plaintext_block
+        # Reset the block
+        previous_block = current_block
 
     return(plaintext)
 
@@ -32,3 +107,40 @@ a_key = b'SECRETSAREHIDDEN'
 a_IV = b'WE KNOW THE GAME'
 assert CBC_decrypt(a_ciphertext, a_key, a_IV)[:18] == \
     b64decode('eW91IGtub3cgdGhlIHJ1bGVz')
+
+
+
+def ECB_encrypt(plaintext, key):
+    # Moved the padding to CBC to better handle different lengths
+
+    # Initialise the cipher in AES-ECb mode
+    cipher = AES.new(key, AES.MODE_ECB)
+    # Encrypt
+    ciphertext = cipher.encrypt(plaintext)
+
+    return ciphertext
+
+
+
+def CBC_encrypt(plaintext, key, IV):
+
+    ciphertext = b''
+    previous_block = IV
+
+    padding_length = 16 - (len(plaintext) % 16)
+    plaintext += bytes([padding_length]) * padding_length
+
+
+    for i in range(0, len(plaintext), 16):
+        current_block = ciphertext[i:i+16]
+        # XOR current plaintext block with the previous ciphertext block
+        xor_block = repeating_key_xor(current_block, previous_block)
+        # Encrypt the XOR'd block
+        ciphertext_block = ECB_encrypt(xor_block, key)
+        # Append the encrypted block to the ciphertext
+        ciphertext += ciphertext_block
+        # Update the previous block for the next iteration
+        previous_block = ciphertext_block
+
+
+    return ciphertext
