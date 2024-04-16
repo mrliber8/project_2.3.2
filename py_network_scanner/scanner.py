6ce9cb2f8f820c173a7d02133a46a7fc06e1d3ed
mrliber8 <patricktenbrinke@gmail.com>
2024-04-16 23:14:43 +0200

    UDP scan en IPV6 check toegevoegd

--- py_network_scanner/scanner.py
+++ py_network_scanner/scanner.py
@@ -1,4 +1,5 @@
 import argparse
+import ipaddress
 import nmap
 import pyfiglet
 import scapy.all as scapy
@@ -39,9 +40,23 @@ def main():
 
     # Check the subnet
     target = args.target
-    if '/' not in target:  # Check if subnet is included
-        # If no subnet, add the /32 subnet so it only scans one host
-        target = target + '/32'
+    try:
+        # probeer te kijken of het IP adres correct is
+        network = ipaddress.ip_network(target, strict=False)
+    except ValueError:
+        # Als het geen geldig netwerk of IP adres is, probeer het dan aan te passen
+        try:
+            ip = ipaddress.ip_address(target)
+            # Als het ipv4 is, voeg dan /32 toe voor 1 host scan
+            if ip.version == 4:
+                target = str(ip) + '/32'
+            # Als het ipv6 is, voeg dan /128 toe voor 1 host scan
+            elif ip.version == 6:
+                target = str(ip) + '/128'
+        except ValueError:
+            # Niet een geldig IP of netwerk adres, geef een foutmelding
+            sys.exit(f"Ongeldig IP-adres of subnet: {target}")
+
     
     print(Fore.MAGENTA + 'Starting the scan for the subnet: ', target)
     print("-----------------------------------------------------------------------------------")
@@ -65,17 +80,21 @@ def main():
     else:
         service = True
 
+    print(ip_list)
+
     for ip in ip_list:
         open_ports = []
         closed_ports = []
         services = []
+        port_types = []
 
         #port_list = [21221, 31016, 5601, 10051]
         port_list = [22, 80]
         print(Fore.GREEN)
         print("-----------------------------------------------------------------------------------")
-        for port in port_list:
-        #for port in range(1, 1025):
+        #for port in port_list:
+        '''
+        for port in range(1, 11000):
                 status, service_name = port_and_service_scan(ip, port, service)
                 if status and service:
                     open_ports.append(str(port))
@@ -86,7 +105,25 @@ def main():
                     print("ip: ", ip, "port: ", port)
                 elif not status:
                     closed_ports.append(str(port))
-
+        '''
+        # UDP Scan toegevoegd en de if statements voor het printen mooier gemaakt
+        for port in range(1, 500):
+            # TCP Scan
+            status, service_name, port_type = port_and_service_scan(ip, port, service, use_udp=False)
+            if status:
+                open_ports.append(str(port))
+                services.append(service_name)
+                port_types.append(port_type)
+                print(f"ip: {ip}, port: {port}, type: {port_type}, service: {service_name if service else 'No service detected'}")
+            
+            # UDP Scan
+            status, service_name, port_type = port_and_service_scan(ip, port, service, use_udp=True)
+            if status:
+                open_ports.append(str(port))
+                services.append(service_name)
+                port_types.append(port_type)
+                print(f"ip: {ip}, port: {port}, type: {port_type}, service: {service_name if service else 'No service detected'}")
+            
     print(Fore.CYAN)
     print("-----------------------------------------------------------------------------------")
     if args.mac:
@@ -142,15 +179,23 @@ def hostname(ip):
 
 
 # 2.c and d, open ports and the services that belong to it
-def port_and_service_scan(ip, port, service):
+def port_and_service_scan(ip, port, service, use_udp=False):
     # Make a new socket object
         # AF_INET is IPV4
         # SOCK_STREAM is TCP
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # connect_ex() returns 0 if it can connect
+
+    if use_udp:
+        # Use UDP socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    else:
+        # Use TCP socket as default
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
     socket.setdefaulttimeout(1)
     result = s.connect_ex((ip, port))
-
+    '''
     if result == 0 and service:
         try:
             service_name = socket.getservbyport(port)
@@ -159,11 +204,19 @@ def port_and_service_scan(ip, port, service):
         return True, service_name
     elif result == 0:
         return True, None
-
+    '''
+    if result == 0 and service:
+        try:
+            service_name = socket.getservbyport(port, 'udp' if use_udp else 'tcp')
+        except:
+            service_name = 'Unknown service'
+        return True, service_name, 'UDP' if use_udp else 'TCP'
+    elif result == 0:
+        return True, None, 'UDP' if use_udp else 'TCP'
 
     s.close()
-
-    return False, None
+    return False, None, 'UDP' if use_udp else 'TCP'
+    #return False, None
 
 
 def scan_ip_and_mac(target, mac):
@@ -178,8 +231,8 @@ def scan_ip_and_mac(target, mac):
 
     # Send the Arp request
     #answer, no_answer = scapy.srp(arp_request_broadcast, timeout=5, iface="WAN Miniport (Network Monitor)")
-    answer, no_answer = srp(arp_request_broadcast, timeout=30, iface="VMware Virtual Ethernet Adapter for VMnet8")
-    #answer, no_answer = srp(arp_request_broadcast, timeout=10)
+    #answer, no_answer = srp(arp_request_broadcast, timeout=30, iface="VMware Virtual Ethernet Adapter for VMnet8")
+    answer, no_answer = srp(arp_request_broadcast, timeout=10, iface="Realtek PCIe GBE Family Controller")
 
     print("answer: ", answer)
     print("no_answer: ", no_answer)
@@ -210,10 +263,10 @@ def printart():
   T = ("LN: 376303")
   ASCII_art_1 = pyfiglet.figlet_format(T)
   print(ASCII_art_1)
-
+  '''
   T = ("p.r.ten.brinke@st.hanze.nl")
   ASCII_art_1 = pyfiglet.figlet_format(T)
   print(ASCII_art_1)    
-
+  '''
 
 main()
